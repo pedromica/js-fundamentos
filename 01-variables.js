c15aa412c5b28b7fede7706931421336a3399527
pedromica <pedromicanovas@gmail.com>
2023-05-10 16:40:24 +0200

    create variables session 01

---
+++ 01-variables.js
@@ -0,0 +1,52 @@
+/** Cadena de texto   */
+
+"Hola que tal"     
+
+'Hola que tal'
+
+"hi don't do ig"
+
+// Para delcarar una variable; 
+    var master = "Rock{TheCode}";
+//  var = hacemos la variable
+//      master = nombre de la variable que hemos creado o declarado.¡
+//               "Rock{theCode}" = valor de la variable que hemos declarado en  este caso es "string".
+// Podemos reagignar su valor usando el símbolo = con una variable ya declarada
+    master = "Rock{TheCode} Reloaded";
+    
+
+
+/* Variable: es una caja que meto un valor lo almaceno y puedo compartir */
+
+var greetings = 'hola desde'
+var school = 'mi casa'
+
+var greetingsFormSchool = greetings+ ' ' +  school;
+
+console.log(greetingsFormSchool);
+
+/*
+Al nombrar la variable, debemos intentar que sea lo más descriptiva posible, utilizaremos camelCase, y usaremos siempre el inglés, ya que probablemente trabajes con gente de diferentes países del mundo
+
+ */
+
+// holaQueTal (camelCase); Forma principal para nombrar variables JavaScript */
+// FINAL_TEXT (PASCAL_CASE); Para valores que nunca cambiamos muy estaticos en el codigo 
+
+// string
+// number
+// boolan
+// array
+// objet 
+
+var myString = "hola";
+var myNumber = 10;
+var MyBoolean = true;
+var myArray = ["Hola", 10, true];
+var myObject = {
+    name: "Pedromi",
+    surname: "diez",
+    edad: 40,
+}
+
+
